3412998a34a2427b6b896b8c2b58376649098f59
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2022-10-30 12:41:04 -0400

    Added classes for Ship Constraints
    Also wrote some corresponding tests

--- battle.py
+++ battle.py
@@ -1,9 +1,11 @@
+from abc import ABC, abstractmethod
+from enum import Enum
 import sys
 from typing import *
 
 
 Grid = List[List[str]]
-Coord = Tuple[int, int]
+Cell = Tuple[int, int]
 
 
 input_chars = {
@@ -16,6 +18,146 @@ input_chars = {
     'B',
     'M'
 }
+
+
+class Piece(Enum):
+    # Water
+    Water = 0
+    # Submarine (1x1)
+    Sub = 1
+
+    # Destroyer (1x2)
+    D_H_S = 2
+    D_H_E = 3
+    D_V_S = 4
+    D_V_E = 5
+
+    # Cruiser (1x3)
+    C_H_S = 6
+    C_H_E = 7
+    C_M = 8
+    C_V_S = 9
+    C_V_E = 10
+
+    # Cruiser (1x3)
+    B_H_S = 11
+    B_H_E = 12
+    B_M = 13
+    B_V_S = 14
+    B_V_E = 15
+
+
+class Constraint(ABC):
+
+    scope: List[Cell] = []
+
+    @abstractmethod
+    # The order of variable assignments in parameter <assignment>
+    # must match order of variables in scope
+    def is_satisfied(self, assignment: List[Piece]) -> bool:
+        pass
+
+
+class HorizontalConstraint(Constraint):
+
+    def __init__(self, cells: List[Cell]) -> None:
+
+        row = cells[0][0]
+        prev_col = cells[0][1] - 1
+
+        for cell in cells:
+            if cell[0] != row:
+                raise Exception("All Cells must be on the same row")
+            if prev_col + 1 != cell[1]:
+                raise Exception("All Cells must be contiguous")
+            prev_col = cell[1]
+
+        self.scope = cells
+
+
+class VerticalConstraint(Constraint):
+
+    def __init__(self, cells: List[Cell]) -> None:
+
+        col = cells[0][1]
+        prev_row = cells[0][0] - 1
+
+        for cell in cells:
+            if cell[1] != col:
+                raise Exception("All Cells must be on the same column")
+            if prev_row + 1 != cell[0]:
+                raise Exception("All Cells must be contiguous")
+            prev_row = cell[0]
+
+        self.scope = cells
+
+
+class DestroyerHorizontal(HorizontalConstraint):
+
+    def is_satisfied(self, assignment: List[Piece]) -> bool:
+        if len(assignment) != 2:
+            raise Exception("Invalid (horizontal) Destroyer Assignment")
+
+        if assignment[0] != Piece.D_H_S:
+            return True
+        return assignment[1] == Piece.D_H_E;
+
+
+class DestroyerVertical(VerticalConstraint):
+
+    def is_satisfied(self, assignment: List[Piece]) -> bool:
+        if len(assignment) != 2:
+            raise Exception("Invalid (vertical) Destroyer Assignment")
+
+        if assignment[0] != Piece.D_V_S:
+            return True
+        return assignment[1] == Piece.D_V_E;
+
+
+class CruiserHorizontal(HorizontalConstraint):
+
+    def is_satisfied(self, assignment: List[Piece]) -> bool:
+        if len(assignment) != 3:
+            raise Exception("Invalid (horizontal) Cruiser Assignment")
+
+        if assignment[0] != Piece.C_H_S:
+            return True
+        return assignment[1] == Piece.C_M and assignment[2] == Piece.C_H_E;
+
+
+class CruiserVertical(VerticalConstraint):
+
+    def is_satisfied(self, assignment: List[Piece]) -> bool:
+        if len(assignment) != 3:
+            raise Exception("Invalid (vertical) Cruiser Assignment")
+
+        if assignment[0] != Piece.C_V_S:
+            return True
+        return assignment[1] == Piece.C_M and assignment[2] == Piece.C_V_E;
+
+
+class BattleshipHorizontal(HorizontalConstraint):
+
+    def is_satisfied(self, assignment: List[Piece]) -> bool:
+        if len(assignment) != 4:
+            raise Exception("Invalid (horizontal) Battleship Assignment")
+
+        if assignment[0] != Piece.B_H_S:
+            return True
+        return assignment[1] == Piece.B_M and assignment[2] == Piece.B_M \
+            and assignment[3] == Piece.B_H_E;
+
+
+class BattleshipVertical(VerticalConstraint):
+
+    def is_satisfied(self, assignment: List[Piece]) -> bool:
+        if len(assignment) != 4:
+            raise Exception("Invalid (vertical) Battleship Assignment")
+
+        if assignment[0] != Piece.B_V_S:
+            return True
+        return assignment[1] == Piece.B_M and assignment[2] == Piece.B_M \
+            and assignment[3] == Piece.B_V_E;
 
 
 def read_input(
---
+++ test_constraint.py
@@ -0,0 +1,231 @@
+import unittest
+from battle import *
+
+
+class TestDestroyerHorizontal(unittest.TestCase):
+
+    def test_vacuous_assignment(self):
+        assignment = [Piece.C_H_S, Piece.C_M]
+        sut = DestroyerHorizontal([(0, 0), (0, 1)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_ideal_assignment(self):
+        assignment = [Piece.D_H_S, Piece.D_H_E]
+        sut = DestroyerHorizontal([(0, 0), (0, 1)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_invalid_assignment1(self):
+        assignment = [Piece.D_H_S, Piece.Water]
+        sut = DestroyerHorizontal([(0, 0), (0, 1)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_invalid_assignment2(self):
+        assignment = [Piece.D_H_S, Piece.D_V_E]
+        sut = DestroyerHorizontal([(0, 0), (0, 1)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+
+class TestDestroyerVertical(unittest.TestCase):
+
+    def test_vacuous_assignment(self):
+        assignment = [Piece.C_V_S, Piece.C_M]
+        sut = DestroyerVertical([(0, 0), (1, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_ideal_assignment(self):
+        assignment = [Piece.D_V_S, Piece.D_V_E]
+        sut = DestroyerVertical([(0, 0), (1, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_invalid_assignment1(self):
+        assignment = [Piece.D_V_S, Piece.Water]
+        sut = DestroyerVertical([(0, 0), (1, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_invalid_assignment2(self):
+        assignment = [Piece.D_V_S, Piece.D_H_E]
+        sut = DestroyerVertical([(0, 0), (1, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+
+class TestCruiserHorizontal(unittest.TestCase):
+
+    def test_vacuous_assignment(self):
+        assignment = [Piece.B_H_S, Piece.B_M, Piece.B_M]
+        sut = CruiserHorizontal([(0, 0), (0, 1), (0, 2)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_ideal_assignment(self):
+        assignment = [Piece.C_H_S, Piece.C_M, Piece.C_H_E]
+        sut = CruiserHorizontal([(0, 0), (0, 1), (0, 2)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_missing_middle(self):
+        assignment = [Piece.C_H_S, Piece.Water, Piece.C_H_E]
+        sut = CruiserHorizontal([(0, 0), (0, 1), (0, 2)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_missing_end(self):
+        assignment = [Piece.C_H_S, Piece.C_M, Piece.Water]
+        sut = CruiserHorizontal([(0, 0), (0, 1), (0, 2)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+
+class TestCruiserVertical(unittest.TestCase):
+
+    def test_vacuous_assignment(self):
+        assignment = [Piece.B_V_S, Piece.B_M, Piece.B_M]
+        sut = CruiserVertical([(0, 0), (1, 0), (2, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_ideal_assignment(self):
+        assignment = [Piece.C_V_S, Piece.C_M, Piece.C_V_E]
+        sut = CruiserVertical([(0, 0), (1, 0), (2, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_missing_middle(self):
+        assignment = [Piece.C_V_S, Piece.Water, Piece.C_V_E]
+        sut = CruiserVertical([(0, 0), (1, 0), (2, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_missing_end(self):
+        assignment = [Piece.C_V_S, Piece.C_M, Piece.Water]
+        sut = CruiserVertical([(0, 0), (1, 0), (2, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+
+class TestBattleshipHorizontal(unittest.TestCase):
+
+    def test_vacuous_assignment(self):
+        assignment = [Piece.Sub, Piece.Water, Piece.Water, Piece.Sub]
+        sut = BattleshipHorizontal([(0, 0), (0, 1), (0, 2), (0, 3)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_ideal_assignment(self):
+        assignment = [Piece.B_H_S, Piece.B_M, Piece.B_M, Piece.B_H_E]
+        sut = BattleshipHorizontal([(0, 0), (0, 1), (0, 2), (0, 3)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_missing_middle1(self):
+        assignment = [Piece.B_H_S, Piece.Water, Piece.B_M, Piece.B_H_E]
+        sut = BattleshipHorizontal([(0, 0), (0, 1), (0, 2), (0, 3)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_missing_middle2(self):
+        assignment = [Piece.B_H_S, Piece.B_M, Piece.Water, Piece.B_H_E]
+        sut = BattleshipHorizontal([(0, 0), (0, 1), (0, 2), (0, 3)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_missing_end(self):
+        assignment = [Piece.B_H_S, Piece.B_M, Piece.B_M, Piece.Water]
+        sut = BattleshipHorizontal([(0, 0), (0, 1), (0, 2), (0, 3)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+
+class TestBattleshipVertical(unittest.TestCase):
+
+    def test_vacuous_assignment(self):
+        assignment = [Piece.Sub, Piece.Water, Piece.Water, Piece.Sub]
+        sut = BattleshipVertical([(0, 0), (1, 0), (2, 0), (3, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_ideal_assignment(self):
+        assignment = [Piece.B_V_S, Piece.B_M, Piece.B_M, Piece.B_V_E]
+        sut = BattleshipVertical([(0, 0), (1, 0), (2, 0), (3, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertTrue(result)
+
+    def test_missing_middle1(self):
+        assignment = [Piece.B_V_S, Piece.Water, Piece.B_M, Piece.B_V_E]
+        sut = BattleshipVertical([(0, 0), (1, 0), (2, 0), (3, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_missing_middle2(self):
+        assignment = [Piece.B_V_S, Piece.B_M, Piece.Water, Piece.B_V_E]
+        sut = BattleshipVertical([(0, 0), (1, 0), (2, 0), (3, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+    def test_missing_end(self):
+        assignment = [Piece.B_V_S, Piece.B_M, Piece.B_M, Piece.Water]
+        sut = BattleshipVertical([(0, 0), (1, 0), (2, 0), (3, 0)])
+
+        result = sut.is_satisfied(assignment)
+
+        self.assertFalse(result)
+
+if __name__ == "__main__":
+    unittest.main()
